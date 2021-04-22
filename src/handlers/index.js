fb3b12980d0fa2993c52f6712f0bd49d016a3944
emekauja <60346432+emekauja@users.noreply.github.com>
2021-04-23 00:49:24 +0100

    "created redirect handler functions"

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,8 +1,25 @@
 module.exports = async (req, res, next) => {
-  const { id: shortUrl } = req.params;
-
   try {
+    //const { id: shortUrl } = req.params;
+
+    // Get URL
+    const address = req.params.id.replace('+', '');
+    const url = db.url.findOne({ shortUrl: address }).dataValues;
+
+    if (url) {
+      // Create clickCount
+      //url.clickCount++;
+      url.clickCount.add(1);
+      await url.save();
+
+      // Redirect to orignal url
+      return res.redirect(url.longUrl);
+    } else {
+      // 3. When no link, if has domain redirect to domain's homepage
+      // otherwise rediredt to 404
+      return res.redirect(301, domain ? domain.homepage : '/404');
+    }
   } catch (error) {
-    return res.status(500);
+    return res.status(500).send(error);
   }
 };
