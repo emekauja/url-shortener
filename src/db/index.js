e701ac4d48f2b3921c16bbad2b3e23d4f03112b9
emekauja <60346432+emekauja@users.noreply.github.com>
2021-05-04 19:48:26 +0100

    "dialect and logging bug fic"

--- src/db/index.js
+++ src/db/index.js
@@ -23,10 +23,9 @@ const sequelize = new Sequelize(
     db: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'postgres' || process.env.DB_DIALECT,
     schema: process.env.DB_SCHEMA,
     query: { raw: true },
-    logging: console.log,
   }
 );
 console.log(
